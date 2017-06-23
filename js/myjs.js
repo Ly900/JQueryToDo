2f9df14c837d18a90e00c36dc0f7984d0d6ff82d
Ly Nguyen <Ly900@aol.com>
2017-06-22 20:05:54 -0400

    Document functions.

--- js/myjs.js
+++ js/myjs.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     var $errorMsg = $("p");
     var $toDoHelper = $("div.to-do-helper");
 
+    // Adds new to do list item to ordered list.
     $addBtn.on("click", function() {
         var newToDo = $toDoInput.val();
         if (!checkEmptyInput()) {
@@ -13,6 +14,7 @@ $(document).ready(function() {
         }
     })
 
+    // Pressing Enter from keyboard adds to do.
     $toDoInput.keypress(function(event) {
         if (event.keyCode == 13) {
             $addBtn.click();
@@ -20,70 +22,82 @@ $(document).ready(function() {
         }
     })
 
+    // Clicking on To Do input.
     $toDoInput.on("click", function() {
-      var input = $(this);
-      clearToDoInput(input);
-      animateInputBorder(input);
-      moveToDoHelper();
+        var input = $(this);
+        clearToDoInput(input);
+        animateInputBorder(input);
+        moveToDoHelper();
     })
 
+    // Clicking on To Do List item.
+    $toDoUl.on("click", "li.to-do", function() {
+        var toDoItem = $(this);
+        removeToDo(toDoItem);
+    });
+
+    // Clears to do input.
     function clearToDoInput(input) {
-      input.attr("placeholder", "");
+        input.attr("placeholder", "");
     }
 
+    // Moves hidden "New To Do" words above input area.
     function moveToDoHelper() {
-      $toDoHelper.addClass("animated");
+        $toDoHelper.addClass("animated");
     }
 
+    // Animates input's bottom border when clicked on.
     function animateInputBorder(input) {
-      input.addClass("animated-border");
+        input.addClass("animated-border");
     }
 
+    // When the user clicks anywhere off the input.
     $toDoInput.blur(function() {
-      var input = $(this);
-      hideToDoHelper();
-      undoClearInput(input);
-      unanimateBorder(input);
+        var input = $(this);
+        hideToDoHelper();
+        undoClearInput(input);
+        unanimateBorder(input);
     })
 
+    // Adds placeholder back into cleared input.
     function undoClearInput(input) {
-      input.attr("placeholder", "New To Do");
+        input.attr("placeholder", "New To Do");
     }
 
+    // Removes animation from input's bottom border.
     function unanimateBorder(input) {
-      input.removeClass("animated-border");
+        input.removeClass("animated-border");
     }
 
+    // Hides "New To Do" animation under input again.
     function hideToDoHelper() {
-      $toDoHelper.removeClass("animated");
+        $toDoHelper.removeClass("animated");
     }
 
+    // Adds a new list item with to do captured from input.
     function addToDo(newToDo) {
         var newLi = "<li class='to-do'>" + newToDo + "</li>";
         $toDoUl.append(newLi);
         $toDoInput.val("");
     }
 
-    function removeToDo() {
-        $toDoUl.on("click", "li.to-do", function() {
-            $(this).remove();
-        })
+    // Removes to do list item from ordered list.
+    function removeToDo(toDoItem) {
+        $(toDoItem).remove();
     }
 
+    // Shows a hidden error message if an empty to do input is submitted.
     function checkEmptyInput() {
         if ($toDoInput.val() == "") {
             $errorMsg.fadeTo(0, 1);
-            setTimeout(showMsgBack, 1000);
+            setTimeout(function showMsgBack() {
+                $errorMsg.fadeTo(500, 0);
+            }, 1000);
             return true;
         }
         return false;
 
     }
-
-    function showMsgBack() {
-        $errorMsg.fadeTo(500, 0);
-    }
-
 
     removeToDo();
 
