552c8a8f533f7eb221c3f63fe5ea56f9991d57fd
Ly Nguyen <Ly900@aol.com>
2017-06-26 20:07:49 -0400

    fix conflict

--- index.js
+++ index.js
@@ -2,18 +2,7 @@
 
 var mysql = require('mysql')
 
-<<<<<<< HEAD
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'to_do_user',
-  password : 'to_do_user',
-  database : 'jquery_to_do'
-});
-
-connection.connect()
-=======
 connection.connect();
->>>>>>> 5f73ebf9cae9e49de223d8526187db38a14157d9
 
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
@@ -21,8 +10,4 @@ connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   console.log('The solution is: ', rows[0].solution)
 })
 
-<<<<<<< HEAD
 connection.end()
-=======
-connection.end();
->>>>>>> 5f73ebf9cae9e49de223d8526187db38a14157d9
