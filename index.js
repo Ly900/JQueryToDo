6dffcec9b591348bf812b009914467ca1ca0e335
Ly Nguyen <Ly900@aol.com>
2017-06-26 21:29:53 -0400

    truncate table at beginning and insert into table sequentially

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var express = require('express');
 var mysql = require('mysql');
 var config = require('./config/config.js');
 
@@ -8,19 +9,47 @@ var dbUser = config.dbUser;
 var dbUserPW = config.dbUserPW;
 var dbName = config.dbName;
 
-var connection = mysql.createConnection({
-  host     : dbHost,
-  user     : dbUser,
-  password : dbUserPW,
-  database : dbName
+var con = mysql.createConnection({
+  host: dbHost,
+  user: dbUser,
+  password: dbUserPW,
+  database: dbName
 });
 
-connection.connect();
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+
+var sql1 = 'SELECT * FROM to_do_table';
+var sql2 = 'INSERT INTO to_do_table (to_do_body) VALUES (\"Exercise3.\")';
+var sql3 = 'INSERT INTO to_do_table (to_do_body) VALUES (\"Wash dishes.\")';
+
+con.query('TRUNCATE TABLE to_do_table');
+
+con.query(sql1, function(err, rows, fields) {
   if (err) throw err
+  console.log("Result beginning: " + rows);
+});
 
-  console.log('The solution is: ', rows[0].solution)
-})
+con.query(sql2, function(err, rows, fields) {
+  if (err) throw err;
+});
 
-connection.end()
+con.query(sql3, function(err, rows, fields) {
+  if (err) throw err;
+});
+
+con.query(sql1, function(err, rows, fields) {
+  if (err) throw err;
+
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    console.log("Rows after: " + row.to_do_body);
+  }
+
+});
+
+
+con.end()
